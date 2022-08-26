4edc27f83a9457d509af1167aef707d632c6e39d
gorkem <grkm@gorkem-MacBook-Air.local>
2022-08-26 19:50:38 +0300

    first commit node

---
+++ node/src/Node.java
@@ -0,0 +1,30 @@
+public class Node {
+
+    private String data;
+    private Node next;
+
+    public Node(String data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public Node getNextNode() {
+        return this.next;
+    }
+
+    public void setNextNode(Node next) {
+        this.next = next;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
